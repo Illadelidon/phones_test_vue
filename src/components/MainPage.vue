1187f0448d757cc3594fbdc7289fa577fddcf1f3
Ilyadelidon <Illadelidon95@gmail.com>
2024-09-22 13:34:48 +0300

    add contact

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,59 +2,31 @@
   <div>
     <div>
       <Form :validation-schema="validationSchema" :validate-on-mount="false" :validateOnChange="true">
-      <!-- Поля для введення імені та прізвища -->
-      <label for="first_name"></label>
-<!--        <Field id="first_name" type="text" name="first_name"  />
-        <ErrorMessage name="first_name" class="error"/>
-
-        <label for="second_name"></label>
-        <Field id="second_name" type="text" name="second_name"  />
-        <ErrorMessage name="second_name" class="error"/>-->
-
-
-
-        <Field id="first_name" type="text" name="first_name" placeholder="First Name" v-model="form.first_name"/>
+        <!-- Поля для введення імені та прізвища -->
+        <Field id="first_name" type="text" name="first_name" placeholder="First Name" v-model="form.first_name" />
         <ErrorMessage name="first_name" class="error" />
 
-<!--      <input id="first_name" name="first_name" v-model="form.first_name" placeholder="First Name" />-->
-        <Field id="last_name" type="text" name="last_name" placeholder="Last Name" v-model="form.last_name"/>
+        <Field id="last_name" type="text" name="last_name" placeholder="Last Name" v-model="form.last_name" />
         <ErrorMessage name="last_name" class="error" />
-<!--      <input v-model="form.last_name" placeholder="Last Name" />-->
 
+        <!-- Поля для введення телефонів -->
+        <div v-for="(phone, index) in form.phone_numbers" :key="index" style="margin-bottom: 10px;">
+          <input
+              v-model="form.phone_numbers[index]"
+              @input="checkPhoneExists(index)"
+              placeholder="Phone Number"
+              style="width: 200px;"
+          />
+          <ErrorMessage name="phone_numbers" class="error" />
+          <button @click="addPhone" v-if="index === form.phone_numbers.length - 1">+</button>
+          <button @click="removePhone(index)" v-if="form.phone_numbers.length > 1">Remove</button>
+          <p v-if="phoneErrors[index]">This phone number already exists in the database.</p>
+        </div>
 
-
-      <!-- Поля для введення телефонів -->
-      <div v-for="(phone, index) in form.phone_numbers" :key="index" style="margin-bottom: 10px;">
-
-<!--        <Field
-            style="width: 200px;"
-            :id="'phone_number_' + index"
-            type="text"
-            :name="'phone_numbers[' + index + ']'"
-            placeholder="Phone Number"
-            v-model="form.phone_numbers[index]"
-            @input="checkPhoneExists(index)"
-        />-->
-
-        <input
-            v-model="form.phone_numbers[index]"
-            @input="checkPhoneExists(index)"
-            placeholder="Phone Number"
-            style="width: 200px;"
-        />
-        <ErrorMessage name="phone_numbers" class="error" />
-        <!-- Кнопка "+" для додавання нового поля -->
-        <button @click="addPhone" v-if="index === form.phone_numbers.length - 1">+</button>
-
-        <!-- Кнопка для видалення поля (з'являється, якщо більше одного поля) -->
-        <button @click="removePhone(index)" v-if="form.phone_numbers.length > 1">Remove</button>
-
-        <!-- Повідомлення про дублікати -->
-        <p v-if="phoneErrors[index]">This phone number already exists in the database.</p>
-      </div>
-
-      <!-- Кнопка для сабміту форми -->
-      <button @click="submitForm" :disabled="hasPhoneErrors">{{ form.id ? 'Update Contact' : 'Submit' }}</button>
+        <div>
+          <button v-if="!form.id" @click="submitForm" :disabled="hasPhoneErrors">Submit</button>
+          <button v-else @click="updateContact" :disabled="hasPhoneErrors">Update Contact</button>
+        </div>
       </Form>
     </div>
 
@@ -105,17 +77,13 @@
 </template>
 
 <script>
-
-import {toTypedSchema} from "@vee-validate/zod";
-import {ErrorMessage, Field, Form} from "vee-validate";
-import {z} from "zod";
-
-
+import { toTypedSchema } from "@vee-validate/zod";
+import { ErrorMessage, Field, Form } from "vee-validate";
+import { z } from "zod";
 
 export default {
-
   name: 'MainPage',
-  components: {ErrorMessage, Field, Form},
+  components: { ErrorMessage, Field, Form },
   data() {
     return {
       contacts: [],
@@ -128,28 +96,23 @@ export default {
         next_page_url: null
       },
       form: {
-        id: null, // Додаємо для редагування
+        id: null,
         first_name: '',
         last_name: '',
         phone_numbers: ['']
       },
-      phoneErrors: [], // Для зберігання помилок при перевірці телефонів
-      hasPhoneErrors: false // Флаг для блокування сабміту форми, якщо є помилки
+      phoneErrors: [],
+      hasPhoneErrors: false
     };
-
   },
   setup() {
     const validationSchema = toTypedSchema(z.object({
       first_name: z.string().min(2, "This is required"),
       last_name: z.string().min(2, "This is required"),
-
     }));
 
-    return {
-      validationSchema,
-    };
+    return { validationSchema };
   },
-
   created() {
     this.fetchContacts();
   },
@@ -158,9 +121,6 @@ export default {
       try {
         const response = await fetch(`http://localhost:8000/api/all-contacts?page=${page}`);
         const data = await response.json();
-        console.log('Fetched contacts:', data);
-
-        // Оновлюємо список контактів і дані пагінації
         if (data.data && Array.isArray(data.data)) {
           this.contacts = data.data;
           this.pagination = {
@@ -191,27 +151,17 @@ export default {
       try {
         const response = await fetch(`http://localhost:8000/api/check-phone?phone=${phone}`);
         const result = await response.json();
-
-        if (result && result.exists) {
-          this.phoneErrors[index] = true;
-          this.hasPhoneErrors = true;
-        } else {
-          this.phoneErrors[index] = false;
-          this.hasPhoneErrors = false;
-        }
+        this.phoneErrors[index] = result.exists;
+        this.hasPhoneErrors = this.phoneErrors.includes(true);
       } catch (error) {
         console.error('Error checking phone:', error);
       }
     },
     async submitForm() {
-      const url = this.form.id
-          ? `http://localhost:8000/api/contacts/${this.form.id}`
-          : 'http://localhost:8000/api/contacts';
-      const method = this.form.id ? 'PUT' : 'POST';
-
+      const url = 'http://localhost:8000/api/contacts';
       try {
         const response = await fetch(url, {
-          method: method,
+          method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
@@ -225,8 +175,8 @@ export default {
         if (response.ok) {
           const result = await response.json();
           console.log('Form submitted:', result);
-          location.reload();
-
+          //this.resetForm();
+          this.fetchContacts();
         } else {
           const errorDetails = await response.json();
           console.error('Error response:', errorDetails);
@@ -235,15 +185,93 @@ export default {
         console.error('Error submitting form:', error);
       }
     },
+    async updateContact() {
+      const url = `http://localhost:8000/api/contacts/${this.form.id}`;
+      try {
+        const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            first_name: this.form.first_name,
+            last_name: this.form.last_name
+          })
+        });
+
+        if (response.ok) {
+          console.log('Contact updated successfully');
+          this.updatePhoneNumbers(); // Викликаємо оновлення телефонів після успішного оновлення імені
+        } else {
+          const errorDetails = await response.json();
+          console.error('Error updating contact:', errorDetails);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    },
+    async updatePhoneNumbers() {
+      const url = `http://localhost:8000/api/contacts/${this.form.id}/phones`;
+      try {
+        const response = await fetch(url, {
+          method: 'PUT', // або 'POST', якщо створюєш нові номери
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            phone_number: this.form.phone_numbers
+          })
+        });
+
+        if (response.ok) {
+          console.log('Phone numbers updated successfully');
+          this.fetchContacts(); // Після успішного оновлення перезавантажуємо список контактів
+        } else {
+          const errorDetails = await response.json();
+          console.error('Error updating phone numbers:', errorDetails);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    },
+    /*async updateForm() {
+      const url = `http://localhost:8000/api/contacts/${this.form.id}`;
+      try {
+        const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+
+            id: this.form.id,
+            first_name: this.form.first_name,
+            last_name: this.form.last_name,
+            phone_numbers: this.form.phone_numbers
+          })
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          console.log('Form updated:', result);
+          this.resetForm();
+          this.fetchContacts();
+        } else {
+          const errorDetails = await response.json();
+          console.error('Error response:', errorDetails);
+        }
+      } catch (error) {
+        console.error('Error updating form:', error);
+      }
+    },*/
     async deleteContact(id) {
       try {
         const response = await fetch(`http://localhost:8000/api/contacts/${id}`, {
           method: 'DELETE'
         });
-
         if (response.ok) {
           console.log('Contact deleted');
-          this.fetchContacts(); // Оновлюємо список контактів
+          this.fetchContacts();
         } else {
           const errorDetails = await response.json();
           console.error('Error deleting contact:', errorDetails);
@@ -256,7 +284,8 @@ export default {
       this.form.id = contact.id;
       this.form.first_name = contact.first_name;
       this.form.last_name = contact.last_name;
-      this.form.phone_numbers = contact.phone_numbers.map(p => p.phone_number); // Оновлюємо телефони для редагування
+      this.form.phone_numbers = contact.phone_numbers.map(p => p.phone_number);
+      console.log('Contact to edit:', this.form);
     },
     resetForm() {
       this.form = {
@@ -265,17 +294,12 @@ export default {
         last_name: '',
         phone_numbers: ['']
       };
-
     },
-
-
   }
-
 };
 </script>
 
 <style scoped>
-/* Стилі для форми та таблиці */
 .contacts-table {
   margin-top: 20px;
 }
@@ -287,6 +311,7 @@ export default {
 button {
   margin: 5px;
 }
+
 .error {
   color: red;
   font-size: 12px;
